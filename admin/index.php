4d872c9d7c646bfd0978cfd2797c3ae0aba95b18
Victor Colucci <victorcbr@gmail.com>
2013-06-23 20:29:00 -0300

    Estrutura Cadastro

--- admin/index.php
+++ admin/index.php
@@ -16,10 +16,16 @@ if (!isset($_SESSION['user'])) {
 <head>
   <meta charset="ISO-8859-1" />
   <title>Mob2You & Hostnect</title>
+	<!-- Carrega arquivos externos -->
   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
   <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  	<!-- Carrega arquivos CSS e JS -->
+      <link href="../bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="../bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
   <link rel="stylesheet" href="../css/style.css" />
+  
+  <!-- Script Tabs JQuery -->
   <script>
   $(function() {
     $( "#tabs" ).tabs();
@@ -27,39 +33,135 @@ if (!isset($_SESSION['user'])) {
   </script>
 </head>
 <body>
+	<!-- Cabeçalho -->
  <div id="header">
  	<div class="logo"><a href="../admin"><img src="imagens/logo.png"></a></div>
  	<div class="sistema">
  	<b>Versão</b>: Web ERP v1.0<br>
  	<b>Usuário</b>: <?php echo $_SESSION['user']?> <br><a href="sair">Sair</a></div>
  </div>
+ 
+ <!-- Sistema -->
 <div id="tabs">
+
+ <!-- Menu Principal -->
   <ul>
-    <li><a href="#tabs-1">Cadastro</a></li>
-    <li><a href="#tabs-2">Vendas</a></li>
-    <li><a href="#tabs-3">Financeiro</a></li>
-    <li><a href="#tabs-4">Utilitários</a></li>
-    <li><a href="#tabs-5">Ajuda</a></li>
+    <li><a href="#cadastro">Cadastro</a></li>
+    <li><a href="#vendas">Vendas</a></li>
+    <li><a href="#financeiro">Financeiro</a></li>
+    <li><a href="#utilitarios">Utilitários</a></li>
+    <li><a href="#ajuda">Ajuda</a></li>
   </ul>
-  <div id="tabs-1">
-    <p>Vitão, aqui ficará a área de cadastro. Cadastro de usuários, produtos, fornecedores, clientes, adicionar itens ao estoque, cadastro da empresa que utilizará o WEB-ERP e etc...</p>
+  
+  <!-- Conteúdo Menu Cadastro -->
+  <div id="cadastro">
+	<!-- Menu Secuntário Cadastro -->  
+	<ul class="nav nav-pills">
+	    <li class="dropdown all-camera-dropdown">
+	           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	            Clientes
+	           </a>
+	    </li>
+	    <li class="dropdown all-camera-dropdown">
+	           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	            Fornecedores
+	               <b class="caret"></b>
+	           </a>
+	      <ul class="dropdown-menu">
+	            <li data-filter-camera-type="all"><a data-toggle="tab" href="#">HTML5</a></li>
+	            <li data-filter-camera-type="Alpha"><a data-toggle="tab" href="#">PHP</a></li>
+	            <li data-filter-camera-type="Zed"><a data-toggle="tab" href="#">MySQL</a></li>
+	            <li data-filter-camera-type="Bravo"><a data-toggle="tab" href="#">JavaScript</a></li>
+	
+	      </ul>
+	    </li>
+	    <li class="dropdown all-camera-dropdown">
+	           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	            Categorias
+	               <b class="caret"></b>
+	           </a>
+	      <ul class="dropdown-menu">
+	            <li data-filter-camera-type="all"><a data-toggle="tab" href="#">HTML5</a></li>
+	            <li data-filter-camera-type="Alpha"><a data-toggle="tab" href="#">PHP</a></li>
+	            <li data-filter-camera-type="Zed"><a data-toggle="tab" href="#">MySQL</a></li>
+	            <li data-filter-camera-type="Bravo"><a data-toggle="tab" href="#">JavaScript</a></li>
+	
+	      </ul>
+	    </li>
+	    <li class="dropdown all-camera-dropdown">
+	           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	            Produtos
+	               <b class="caret"></b>
+	           </a>
+	      <ul class="dropdown-menu">
+	            <li data-filter-camera-type="all"><a data-toggle="tab" href="#">HTML5</a></li>
+	            <li data-filter-camera-type="Alpha"><a data-toggle="tab" href="#">PHP</a></li>
+	            <li data-filter-camera-type="Zed"><a data-toggle="tab" href="#">MySQL</a></li>
+	            <li data-filter-camera-type="Bravo"><a data-toggle="tab" href="#">JavaScript</a></li>
+	
+	      </ul>
+	    </li>
+	    <li class="dropdown all-camera-dropdown">
+	           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	            Ítens Estoque
+	               <b class="caret"></b>
+	           </a>
+	      <ul class="dropdown-menu">
+	            <li data-filter-camera-type="all"><a data-toggle="tab" href="#">HTML5</a></li>
+	            <li data-filter-camera-type="Alpha"><a data-toggle="tab" href="#">PHP</a></li>
+	            <li data-filter-camera-type="Zed"><a data-toggle="tab" href="#">MySQL</a></li>
+	            <li data-filter-camera-type="Bravo"><a data-toggle="tab" href="#">JavaScript</a></li>
+	
+	      </ul>
+	    </li>
+	    <li class="dropdown all-camera-dropdown">
+	           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	            Usuários
+	               <b class="caret"></b>
+	           </a>
+	      <ul class="dropdown-menu">
+	            <li data-filter-camera-type="all"><a data-toggle="tab" href="#">HTML5</a></li>
+	            <li data-filter-camera-type="Alpha"><a data-toggle="tab" href="#">PHP</a></li>
+	            <li data-filter-camera-type="Zed"><a data-toggle="tab" href="#">MySQL</a></li>
+	            <li data-filter-camera-type="Bravo"><a data-toggle="tab" href="#">JavaScript</a></li>
+	
+	      </ul>
+	    </li>
+	</ul>
   </div>
-  <div id="tabs-2">
+  <!-- Final Menu Secundário Cadastro -->
+ 
+  
+  <!-- Menu Secuntário Vendas -->  
+  <div id="vendas">
     <p>Vitão, aqui ficará a área de vendas (efetuar vendas, orçamentos, lançamentos, movimentações no turno) e etc...</p>
   </div>
-  <div id="tabs-3">
+ 
+ 
+  <!-- Menu Secuntário Financeiro -->
+  <div id="financeiro">
     <p>Vitão, aqui ficará a área do financeiro (consultar pagamentos/recebimentos, lançamento financeiro, plano de contas, relatÃ³rios financeiro) e etc...</p>
     <p>Parte com complexidade média.</p>
   </div>
-  <div id="tabs-4">
+ 
+ 
+  <!-- Menu Secuntário Utilitários -->
+  <div id="utilitarios">
     <p>Vitão, aqui ficará a área de utilitários (calculadora, busca rapida de clientes, produtos e fornecedores, aviso de estoque baixo, aviso de O.S Ã  vencer 90 dias, bakcup do banco de dados, etc)</p>
   </div>
-  <div id="tabs-5">
+ 
+ 
+  <!-- Menu Secuntário Ajuda -->
+  <div id="ajuda">
     <p>Vitão, aqui ficará a área de ajuda aos usuários do WEB-ERP (manual de uso, central de dúvidas, atualização de sistema)</p>
   </div>
-  
-</div>
 
+
+<!-- Fim Menu Secuntário Vendas -->  
+</div>
+<!-- Fim Sistema -->
+
+<!-- Rodapé -->
 <div id="rodape">
 <center><p>Mob2You <?php echo date(Y);?>. Todos os direitos reservados.</p></center>
 </div>
